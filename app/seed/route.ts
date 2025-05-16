9629b0cc51e2ee4a7a3ca3ce746df0025d5b9e35
Pawel Piotrowski <pawel.piotrowski@home.eu>
2025-05-16 14:43:27 +0200

    chapter 14 fix

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -103,14 +103,17 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    // const result = await sql.begin((sql) => [
-    //   seedUsers(),
-    //   seedCustomers(),
-    //   seedInvoices(),
-    //   seedRevenue(),
-    // ]);
+    const result = await sql.begin((sql) => [
+      seedUsers(),
+      seedCustomers(),
+      seedInvoices(),
+      seedRevenue(),
+    ]);
 
-    return Response.json({ message: "Database seeded successfully" });
+    return Response.json({
+      result: result,
+      message: "Database seeded successfully",
+    });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
