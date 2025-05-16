a1d5e2a81d2609cbf416b87460d3d751e317ebfd
Pawel Piotrowski <pawel.piotrowski@home.eu>
2025-05-16 14:39:52 +0200

    chapter 14 lint fixes

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,7 +1,7 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Image from "next/image";
-import { LatestInvoice } from "@/app/lib/definitions";
+// import { LatestInvoice } from "@/app/lib/definitions";
 import { lusitana } from "../fonts";
 import { fetchLatestInvoices } from "@/app/lib/data";
 
--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,6 +1,6 @@
 import { generateYAxis } from "@/app/lib/utils";
 import { CalendarIcon } from "@heroicons/react/24/outline";
-import { Revenue } from "@/app/lib/definitions";
+// import { Revenue } from "@/app/lib/definitions";
 import { lusitana } from "../fonts";
 import { fetchRevenue } from "@/app/lib/data";
 
